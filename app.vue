20016e86799e17774f30813229d6289274d63f5f
Francis <haruka31@gmail.com>
2024-01-04 15:29:05 -0500

    iwi

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtLayout/>
   </div>
 </template>
+<script setup lang="ts">
+useHead({
+  title:"Evisalud",
+  meta:[
+    {
+      name:"Web que brinda cursos de Investigacion"
+    }
+  ]
+})
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,17 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     '@pinia/nuxt',
+    ['@nuxtjs/robots', {
+      UserAgent: '*',
+      Sitemap: (req) => `https://${req.headers.host}/sitemap.xml`
+    }],
   ],
   css: ['@/scss/main.scss'],
-
-  ssr:false,
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
+  ssr: false,
 })
