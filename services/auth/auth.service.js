6837bd2a9592080783ecd478f8df75c88087491c
Francis <haruka31@gmail.com>
2024-01-05 11:46:38 -0500

    up owo

--- services/auth/auth.service.js
+++ services/auth/auth.service.js
@@ -4,7 +4,6 @@ const config = useRuntimeConfig();
 class AuthService {
     async register(body) {
 
-        console.log(body, config);
         try {
             await apiClient.get('/sanctum/csrf-cookie');
             const res = await axios.post(`${config.public.APP_BACKEND_URL}auth/register`, body
