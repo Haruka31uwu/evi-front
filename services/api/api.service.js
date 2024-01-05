9b45dcd44588bf5b81f55dcbec59fb47eef949b0
Francis <haruka31@gmail.com>
2024-01-05 18:44:12 -0500

    owo

--- services/api/api.service.js
+++ services/api/api.service.js
@@ -4,11 +4,7 @@ const config = useRuntimeConfig();
 const apiClient = axios.create({
     baseURL:config.public.APP_BACKEND_URL,
     withCredentials: true,
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        
-    },
+
 });
 
 export default apiClient;
